e17726785f80146dca154f201d6311a9865ffe89
Javier <carriloruiz@gmail.com>
2025-02-19 19:32:12 +0100

    Ejercicios Bucles

--- Programacion Java/Boletin Bucles y Condicionales/src/Ejer3.java
+++ Programacion Java/Boletin Bucles y Condicionales/src/Ejer3.java
@@ -5,5 +5,21 @@ public class Ejer3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        while (true){
+            System.out.println("Introduce un numero");
+           int numeros = sc.nextInt();
+
+            if (numeros == 0){
+                System.out.println("Saliendo del programa");
+                break;
+            }
+            if (numeros % 2 == 0){
+                System.out.println("El numero es par");
+            }else {
+                System.out.println("Es impar");
+            }
+
+        }
+
     }
 }
---
+++ Programacion Java/Boletin Bucles y Condicionales/src/Ejer6.java
@@ -0,0 +1,28 @@
+//Pedir números hasta que se teclee un 0, mostrar la suma de todos los números
+//introducido
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ejer6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int suma = 0;
+        while (true){
+            ArrayList<Integer> numeros = new ArrayList<>();
+            System.out.println("Introduce un numero");
+           int numero = sc.nextInt();
+
+
+            if (numero == 0){
+                System.out.println("Saliendo del programa");
+                break;
+
+            }
+            //Suma toda los numeros introducido
+            suma += numero;
+
+        }
+        System.out.println("La suma total es " + suma);
+    }
+}
