6e35d69a7a1b7e84cc0c2fc8a006042dc9b0ce0b
Javier <carriloruiz@gmail.com>
2025-02-19 16:00:43 +0100

    Bucles

---
+++ Programacion Java/Boletin Bucles y Condicionales/src/Ejer3.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+//Leer números hasta que se introduzca un 0. Para cada uno indicar si es par o impar.
+public class Ejer3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
